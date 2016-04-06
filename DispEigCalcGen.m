e12f96dd1b61f8b69bab8d3cc5e8de873ea3c08a
mstefferson <michael.stefferson@colorado.edu>
2016-04-06 10:34:02 -0600

    Cleaned up DispEigCalcGen for readability. No functional changes

--- DispEigCalcGen.m
+++ DispEigCalcGen.m
@@ -2,40 +2,28 @@ function [eigVecs,eigVals] = ...
     DispEigCalcGen(DiffMobObj,GridObj,ParamObj,...
     kxHolder,kyHolder,Interactions)
 
-
-% keyboard
-
-% Vectors are column vectors
+% Commonly used variables 
 Nm  = ParamObj.Nm;
+km   = GridObj.km;
 kx0holder = ParamObj.Nx/2 +1;
 ky0holder = ParamObj.Ny/2 +1;
+c = pi * ParamObj.bcP / (ParamObj.L_rod ^ 2);
 
-Nc = 10;
 % Calculate coeff
+Nc = 10;
 [Coeff_best, ~] = CoeffCalcExpCos2D(Nc,GridObj.phi,ParamObj.bcE); 
+
 % Build equil distribution
 f = DistBuilderExpCos2Dsing(Nc,GridObj.phi,Coeff_best);        
 f_FT = fftshift( fft( f ) );
 rhoEqlFT = zeros(ParamObj.Nx,ParamObj.Ny,Nm);
-c = pi * ParamObj.bcP / (ParamObj.L_rod ^ 2);
-
-% rhoEqlFT(ParamObj.Nx/2+1,ParamObj.Ny/2+1,:) = c .* f_FT;
-
 
 rhoEqlFT(ParamObj.Nx/2+1,ParamObj.Ny/2+1,:) = ...
     c * ParamObj.Nx * ParamObj.Ny  .* f_FT;    
     
-% rhoEqlTemp = c * ParamObj.Nx * ParamObj.Ny  .* f_FT(Nm/2+1);
-% rhoEqlFT = zeros(Nm,Nm,Nm);
-% rhoEqlFT(ParamObj.Nx/2+1,ParamObj.Ny/2+1,Nm/2+1) = rhoEqlTemp;
-
-
-% keyboard
-
 if 1
     rhoEqlFT =  real(rhoEqlFT);   
 end
-% keyboard
 
 % Prefactors
 MdiagVec = -( DiffMobObj.D_par + DiffMobObj.D_perp ) / 2 .* ...
@@ -43,32 +31,24 @@ MdiagVec = -( DiffMobObj.D_par + DiffMobObj.D_perp ) / 2 .* ...
     - DiffMobObj.D_rot * GridObj.km .^2 ;
 MdiagVec = MdiagVec';
 
-
-% rho_0 = c / (2*pi)
-
-%     Fm = MayerFncDiffBtwPntsCalc(...
-%         ParamObj.Nx, ParamObj.Ny, ParamObj.Nm, ...
-%         ParamObj.Lx, ParamObj.Ly, GridObj.dx,...
-%         GridObj.dy, GridObj.dphi, ParamObj.L_rod);
-  Fm = MayerFncDiffBtwPntsCalc(...
+% Mayer Function
+Fm = MayerFncDiffBtwPntsCalc(...
         ParamObj.Nx, ParamObj.Ny, ParamObj.Nm, ...
         ParamObj.Lx, ParamObj.Ly, ParamObj.L_rod);
     
 Fm_FT = fftshift(fftn( Fm ) );
-    %     toc
-%         keyboard
-    % Divide by 1/N^3 from FT normalization
-    IntPreFac = 2 * pi * ParamObj.Lx * ParamObj.Ly / ...
+
+% Divide by 1/N^3 from FT normalization
+IntPreFac = 2 * pi * ParamObj.Lx * ParamObj.Ly / ...
         ( ParamObj.Nx * ParamObj.Ny * ParamObj.Nm ) ^ 2;
     
-   
 %% Build the Matrix
 posksqr   = ( DiffMobObj.D_par + DiffMobObj.D_perp ) / 2 .* ...
     ( GridObj.kx(kxHolder) .^ 2 + GridObj.ky(kyHolder).^ 2 ) ;
-MintAlpha = zeros(Nm,Nm);
+MintAlpha   = zeros(Nm,Nm);
 MintAlphaP2 = zeros(Nm,Nm);
 MintAlphaM2 = zeros(Nm,Nm);
-MintBeta  = zeros(Nm,Nm);
+MintBeta    = zeros(Nm,Nm);
 
 alphatempP2 = IntPreFac .* (DiffMobObj.D_par - DiffMobObj.D_perp) / 4 * ...
     ( GridObj.kx(kxHolder) + sqrt(-1) * GridObj.ky(kyHolder) ) ^ 2;
@@ -76,8 +56,6 @@ alphatempP2 = IntPreFac .* (DiffMobObj.D_par - DiffMobObj.D_perp) / 4 * ...
 alphatempM2 = IntPreFac .* (DiffMobObj.D_par - DiffMobObj.D_perp) / 4 * ...
     ( GridObj.kx(kxHolder) - sqrt(-1) * GridObj.ky(kyHolder) ) ^ 2;
 
-% keyboard
-km   = GridObj.km;
 for i = 1:Nm;
 
     for j = 1:Nm;
@@ -85,27 +63,14 @@ for i = 1:Nm;
         Alphaholder = Nm / 2 + 1 - j + i;
         AlphaholderP2 = Nm / 2 + 1 - j + i + 2; % exp(m + n - 2)
         AlphaholderM2 = Nm / 2 + 1 - j + i - 2; % exp(m + n + 2)
-        
-        if Alphaholder < 1
-            Alphaholder = Alphaholder + Nm;
-        end
-        if Alphaholder > Nm
-            Alphaholder = Alphaholder - Nm;
-        end
-        
-        if AlphaholderP2 < 1
-            AlphaholderP2 = AlphaholderP2 + Nm;
-        end
-        if AlphaholderP2 > Nm
-            AlphaholderP2 = AlphaholderP2 - Nm;
-        end
-        
-        if AlphaholderM2 < 1
-            AlphaholderM2 = AlphaholderM2 + Nm;
-        end
-        if AlphaholderM2 > Nm
-            AlphaholderM2 = AlphaholderM2 - Nm;
-        end
+       
+        % Wrap index around
+        if Alphaholder < 1;  Alphaholder = Alphaholder + Nm; end;
+        if Alphaholder > Nm; Alphaholder = Alphaholder - Nm; end;
+        if AlphaholderP2 < 1;  AlphaholderP2 = AlphaholderP2 + Nm; end;
+        if AlphaholderP2 > Nm; AlphaholderP2 = AlphaholderP2 - Nm; end;
+        if AlphaholderM2 < 1;  AlphaholderM2 = AlphaholderM2 + Nm; end;
+        if AlphaholderM2 > Nm; AlphaholderM2 = AlphaholderM2 - Nm; end;
         
         alphatemp = IntPreFac .* ...
             (posksqr + DiffMobObj.D_rot .* ...
@@ -122,23 +87,7 @@ for i = 1:Nm;
             Fm_FT(kxHolder,kyHolder,j);
         end
         
-        if Alphaholder == Nm/2 +1
-%             keyboard
-%             IntPreFacTmp = (pi * ParamObj.Lx * ParamObj.Ly * ParamObj.bcP) / ...
-%         ( ParamObj.L_rod^2 * ParamObj.Nx * ParamObj.Ny * ParamObj.Nm );
-%     
-%        InsIntCntr = IntPreFacTmp .*  DiffMobObj.D_rot * GridObj.km(i) .^2 .* ...
-%              Fm_FT(kxHolder,kyHolder,j);
-         
-%          IntPreFacTmp .*  DiffMobObj.D_rot * GridObj.km(i) .^2  - ...
-%              alphatemp .* rhoEqlFT(ParamObj.Nx/2+1,ParamObj.Ny/2+1,Alphaholder);
-        end
-        
-        if i == 207 && j == 207
-%             keyboard
-        end
-        
-        MintAlpha(i,j) = alphatemp .* ...
+       MintAlpha(i,j) = alphatemp .* ...
             rhoEqlFT(ParamObj.Nx/2+1,ParamObj.Ny/2+1,Alphaholder) .* ...
             Fm_FT(kxHolder,kyHolder,j);
 
@@ -158,13 +107,9 @@ for i = 1:Nm;
             rhoEqlFT(kx0holder,ky0holder,Betaholder) .* ...
             Fm_FT(kx0holder,ky0holder,Betaholder );
     end
-    
-%     keyboard
 end
 
-% keyboard
 %% Driving
-
 Mp1PreFac = -ParamObj.vD ./ 2 .* ( ...
     sqrt(-1) .* GridObj.kx(kxHolder) - GridObj.ky(kyHolder) );
 Mm1PreFac = -ParamObj.vD ./ 2 .* ( ...
@@ -172,7 +117,6 @@ Mm1PreFac = -ParamObj.vD ./ 2 .* ( ...
 
 Mp1vec = Mp1PreFac .* ones(Nm,1);
 Mm1vec = Mm1PreFac .* ones(Nm,1);
-
 
 %% Aniso Just diff
 Mp2PreFac =  - ( DiffMobObj.D_par - DiffMobObj.D_perp ) / 4 .* ...
@@ -183,16 +127,15 @@ Mm2PreFac = - ( DiffMobObj.D_par - DiffMobObj.D_perp ) / 4 .* ...
 
 Mp2vec = Mp2PreFac .* ones(Nm,1);
 Mm2vec = Mm2PreFac .* ones(Nm,1);
-%%
 
-% keyboard
 
 if Interactions
     Mint = MintAlpha + MintAlphaP2 + MintAlphaM2 + MintBeta;
 else
     Mint = 0;
 end
-% keyboard 
+
+% Build the Matrix
     M =  diag( MdiagVec') +...
         diag(  Mp1vec(1:Nm-1) ,1) +...
         diag(  Mm1vec(1:Nm-1) ,-1) +...
@@ -209,9 +152,6 @@ end
     M(ParamObj.Nm,2)   = Mp2PreFac;
 
     M = M + Mint;
-   % PBC driving
-    
-%   keyboard 
         
     [eigVecs,eigVals] = eig(M);
 
